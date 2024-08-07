2793d11a0b463f90c47c236d311716f884592e7b
ValkarVarg <mikewilliamstfb@gmail.com>
2024-08-07 16:37:11 +0100

    adds images to project cards

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -4,6 +4,7 @@ export interface ProjectData {
     description: string;
     techStack: string[];
     link: string;
+    image: string;
   }
   
   export const projects: ProjectData[] = [
@@ -13,6 +14,7 @@ export interface ProjectData {
       description: "Allows for API requests to view and update articles, comments, users and topics.",
       techStack: ["SQL", "TDD", "JavaScript"],
       link: "https://nc-news-k0xf.onrender.com/api/",
+      image: "ncimg",
     },
     {
       id: 2,
@@ -20,11 +22,13 @@ export interface ProjectData {
       description: "A news article website allowing users to read and post articles, read and post comments on articles, add new topic categories and vote on article and comments.",
       techStack: ["React", "MUI", "JavaScript"],
       link: "https://valkarncnews.netlify.app/",
+      image: "ncimg",
     },
     {id: 3,
     name: "Gym Bro Hero",
     description: "A workout tracker app with game elements to encourage tracking. Allows the user to create and track workouts, see stats, gain xp and level up. The user can then use their levels to add pets to the home view.",
     techStack: ["React Native", "Expo", "JavaScript", "SQL"],
     link: "https://github.com/ValkarVarg/gymbrohero-be",
+    image: "gymbro",
   },
   ];
